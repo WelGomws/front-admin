a46970285498026b6186e86e2061f99596290a18
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-23 21:54:23 -0300

    feat: export itens accessibility

---
+++ src/accessibilityItems.ts
@@ -0,0 +1,6 @@
+export const accessibilityItems = [
+  { value: "Estação com elevador", label: "Estação com elevador" },
+  { value: "Rampa", label: "Rampa" },
+  { value: "Piso tátil", label: "Piso tátil" },
+  { value: "Sanitário Adaptado PCD", label: "Sanitário Adaptado PCD" },
+];
