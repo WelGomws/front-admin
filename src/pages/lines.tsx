5047ea0aaabd5536ecc8b2802c8851b54d7b48b5
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-24 08:59:32 -0300

    fix: add font

--- src/pages/lines.tsx
+++ src/pages/lines.tsx
@@ -7,6 +7,10 @@ const Home = () => {
     <>
       <Head>
         <title>Portal - Linhas</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Rubik:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Lines />
     </>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,10 @@ import { AuthProvider } from "../contexts/auth";
 const LoginPage = () => (
   <AuthProvider>
     <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Rubik:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       <title>Login</title>
     </Head>
     <Signin />
