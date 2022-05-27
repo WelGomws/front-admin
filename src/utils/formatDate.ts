2c40c3b0c69d1321301be4eb299e234b7ecea362
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-26 21:09:21 -0300

    feat: add hours

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,10 @@
 export const formatDate = (date: string | Date) => {
-  const [year, month, day] = new Date(date)
-    .toISOString()
-    .split("T")[0]
-    .split("-");
-  return `${day}/${month}/${year}`;
+  const dateISO = new Date(date).toISOString();
+  const [hours, minutes, seconds]: any = dateISO
+    .split("T")[1]
+    .split(".")[0]
+    .split(":");
+  const [year, month, day] = dateISO.split("T")[0].split("-");
+
+  return `${day}/${month}/${year} - ${hours - 3}:${minutes}:${seconds}`;
 };
