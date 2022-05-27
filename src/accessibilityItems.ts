9ee3aa83b4530d052e72bca5f5f76312a5abbd73
WelGomws <wellingtondomingos.gomes@gmail.com>
2022-05-26 21:16:44 -0300

    feat: add more items

--- src/accessibilityItems.ts
+++ src/accessibilityItems.ts
@@ -3,4 +3,17 @@ export const accessibilityItems = [
   { value: "Rampa", label: "Rampa" },
   { value: "Piso tátil", label: "Piso tátil" },
   { value: "Sanitário Adaptado PCD", label: "Sanitário Adaptado PCD" },
+  {
+    value: "Transposição Entre Plataforma",
+    label: "Transposição Entre Plataforma",
+  },
+  {
+    value: "Telefone p/ Pessoa em Cadeira de Rodas",
+    label: "Telefone p/ Pessoa em Cadeira de Rodas",
+  },
+  { value: "Sanitário Acessível", label: "Sanitário Acessível" },
+  {
+    value: "Funcionários Especializados",
+    label: "Funcionários Especializados",
+  },
 ];
